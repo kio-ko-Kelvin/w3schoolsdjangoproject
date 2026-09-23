0e0210eee6f3e791a00913158592c985ffb75e5b
Anon <ano54niomous@gmail.com>
2026-09-23 22:01:42 +0300

    Added new rows to the member model

--- members/models.py
+++ members/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length=15)
     lastname = models.CharField(max_length=15)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(auto_now_add=True, null=True)
